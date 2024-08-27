88924faf2618c1bb8ee9afb1a255a0fdc738d860
joongwon <sara970517@gmail.com>
2024-08-27 17:34:17 +0900

    20240827 1.hello java

---
+++ src/CommetJava.java
@@ -0,0 +1,11 @@
+public class CommetJava {
+    public static void main(String[] args) { //"psvm" 단축어
+        System.out.println("hello java1"); //"sout" 단축어
+        //System.out.println("hello java2");
+
+        /* 여러 줄 주석(multi line comment)
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {
+
+    public static void main(String[] args) { //프로그램 시작점
+        System.out.println("hello java");
+    }
+}
