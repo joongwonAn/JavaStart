eccdfc971bce8765aa90730c44735efb9c609de4
joongwon <sara970517@gmail.com>
2024-08-28 13:24:08 +0900

    20240827 1.hello java

---
+++ src/variable/var1.java
@@ -0,0 +1,9 @@
+package variable; // 폴더 이름과 비슷
+public class var1 {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
---
+++ src/variable/var7.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class var7 {
+
+    public static void main(String[] args) {
+        int a =100; // 정수
+        double b = 10.5; // 실수
+        boolean c = true; //불리언(boolean) true, false 입력 가능
+        char d = 'A'; //문자 하나, '작은 따옴표'로 감싸야 함
+        String e = "Hello Java"; // 문자열, 문자열을 다루기 위한 특별한 타입, "큰 따옴표"로 감싸야 함
+
+        System.out.println(a); // ctrl+d = ctrl+c + ctrl+v
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
