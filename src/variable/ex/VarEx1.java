b335e5d05d84a233163721408e7d68650c32e480
joongwon <sara970517@gmail.com>
2024-08-28 13:46:24 +0900

    20240828 2.variable

--- src/variable/ex/VarEx1.java
+++ src/variable/ex/VarEx1.java
@@ -1,5 +1,10 @@
 package variable.ex;
-
+/*
+다음 코드에 반복해서 나오는 숫자 4, 3을 다른 숫자로 한번에 변경할 수 있도록 다음을 변수 num1, num2를 사용하도록 변경해보세요.
+System.out.println(4 + 3);
+System.out.println(4 - 3);
+System.out.println(4 * 3);
+ */
 public class VarEx1 {
     public static void main(String[] args) {
         int num1, num2;
--- src/variable/ex/VarEx3.java
+++ src/variable/ex/VarEx3.java
@@ -1,5 +1,9 @@
 package variable.ex;
-
+/*
+문제3 - long, boolean 데이터 타입
+long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화한 후 출력하는 프로그램을 작성하세요.
+boolean 타입의 변수를 선언하고, 그 변수를 true로 초기화한 후 출력하는 프로그램을 작성하세요.
+*/
 public class VarEx3 {
 
     public static void main(String[] args) {
