2183abdece2eea808f096824a515739183a8ea6d
joongwon <sara970517@gmail.com>
2024-08-28 13:26:19 +0900

    20240828 2.variable

--- src/variable/ex/VarEx3.java
+++ src/variable/ex/VarEx3.java
@@ -4,9 +4,9 @@ public class VarEx3 {
 
     public static void main(String[] args) {
         long longVar = 10000000000L;
-        boolean booleanVar = true;
-
         System.out.println(longVar);
+
+        boolean booleanVar = true;
         System.out.println(booleanVar);
     }
 }
